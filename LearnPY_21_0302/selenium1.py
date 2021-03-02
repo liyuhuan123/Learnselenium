f24879049d2c7bc646dd5d287a381169d7e78b67
liyuhuan123 <2502268134@qq.com>
2021-03-02 15:00:03 +0800

    第一个自动化测试脚本

---
+++ LearnPY_21_0302/selenium1.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+browser = webdriver.Firefox()
+time.sleep(3)
+browser.get("http://www.baidu.com")
+time.sleep(3)
+browser.find_element_by_id("kw").send_keys("益禾堂")
+time.sleep(3)
+browser.find_element_by_id("su").click()
+browser.quit()
